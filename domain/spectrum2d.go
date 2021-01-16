af1b2113033aa5411010a00fb5c5b58b3b1e57a2
flo <floriandavid.lannge@web.de>
2021-01-17 00:54:11 +0100

    - add a dummy test spectrum json - add methods for validity check and time conversion to Spectrum2d

--- domain/spectrum2d.go
+++ domain/spectrum2d.go
@@ -1,5 +1,7 @@
 package domain
 
+import "time"
+
 // Spectrum2d is the representation of a generic two-dimensional experimental dataset with an independent variable(X) and a
 // dependent variable (Y). The MeasuredTime is stored as a unix timestamp. The Id is assigned by the concrete persistence
 // layer whereas the Name is defined by the user.
@@ -7,10 +9,20 @@ type Spectrum2d struct {
 	Id           string
 	Name         string
 	Measurer     string
-	MeasuredTime int
+	MeasuredTime int64 `json:"measured_time"`
 	X            []float64
 	Y            []float64
 }
 
-// todo ensuure MeasuredTime is documented as unix timestamp
-// check if X and Y have the same dimension
+// todo test time conversion
+
+// GetMeasuredTime converts the internal Unix time stamp representation (integer) to the  Go time.Time struct.
+func (spec Spectrum2d) GetMeasuredTime() time.Time {
+	return time.Unix(spec.MeasuredTime, 0)
+}
+
+// IsValid checks if the slices holding the X and Y data have the same size which should be the case for typical
+// experimental data.
+func (spec Spectrum2d) IsValid() bool {
+	return len(spec.X) == len(spec.Y)
+}
